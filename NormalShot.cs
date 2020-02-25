8d608e7531f5a85d72abf105c4deaa0ab3945e4d
marcushodgson <marcushodgson7@gmail.com>
2020-02-25 13:27:58 -0500

    Add files via upload

---
+++ NormalShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalShot : MonoBehaviour
+{
+	public float speed = 20f;
+	public Rigidbody2D rb;
+    public GameObject impactEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo) {
+    	Instantiate(impactEffect, transform.position, transform.rotation);
+    	Destroy(gameObject); 
+
+
+    }
+
+    
+}
